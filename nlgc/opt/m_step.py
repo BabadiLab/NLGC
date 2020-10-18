Fix bugs in calculate_ss and gradient descent

nlgc/opt/m_step.py
@@ -38,7 +38,7 @@ def calculate_ss(x_bar, s_bar, b, m, p):
     """
 
     s_cross = b.dot(s_bar[:, :m])
-    x_ = x_bar[:, (p - 1) * m:]
+    x_ = x_bar[:, :m]
     n = (x_bar.shape[0] - p)
 
     # compute the following quantities carefully
@@ -88,7 +88,6 @@ def solve_for_a(q, s1, s2, a, p1, lambda2, max_iter=5000, tol=1e-3, zeroed_index
             a = linalg.solve(s2, s1.T, assume_a='sym') # s1 * (s2 ** -1)
         return a.T, None
 
-    warnings.filterwarnings('error')
     eps = np.finfo(s1.dtype).eps
     q = np.diag(q)
     qinv = 1 / q
@@ -125,6 +124,7 @@ def solve_for_a(q, s1, s2, a, p1, lambda2, max_iter=5000, tol=1e-3, zeroed_index
         grad = temp1
         grad -= s1
         grad *= qinv
+        grad *= 2
 
         # #************* make the self history = 0 from lag p1***********
         for k in range(p1, p):
@@ -134,6 +134,7 @@ def solve_for_a(q, s1, s2, a, p1, lambda2, max_iter=5000, tol=1e-3, zeroed_index
             grad[zeroed_index] = 0.0
 
         # Find opt step-size
+        warnings.filterwarnings('error')
         try:
             # tau = 0.5 * (grad * grad).sum() / (np.diag(grad.dot(s2.dot(grad.T))) * qinv.ravel()).sum()
             temp2 = grad.dot(s2.T)
@@ -143,6 +144,7 @@ def solve_for_a(q, s1, s2, a, p1, lambda2, max_iter=5000, tol=1e-3, zeroed_index
             tau = max(tau, tau_max)
         except Warning:
             raise RuntimeError(f'Q possibly contains negative value {q.min()}')
+        warnings.filterwarnings('ignore')
 
         while True:
             # Forward step
@@ -164,13 +166,10 @@ def solve_for_a(q, s1, s2, a, p1, lambda2, max_iter=5000, tol=1e-3, zeroed_index
             diff = (a - _a)
             f_new_upper = f_old + (grad * diff).sum() + (diff ** 2).sum() / (2 * tau)
             # print(f_new, f_new_upper)
-            if f_new < f_new_upper or tau / tau_max < 1e-4:
+            if f_new < f_new_upper or tau / tau_max < 1e-10:
                 break
             else:
                 tau /= 2
-        # if a.max() > 1:
-        #     import ipdb;
-        #     ipdb.set_trace()
 
         num = np.sum(diff ** 2)
         den = np.sum(_a ** 2)
@@ -178,8 +177,9 @@ def solve_for_a(q, s1, s2, a, p1, lambda2, max_iter=5000, tol=1e-3, zeroed_index
         changes[i+1] = 1 if den == 0 else np.sqrt(num / den)
 
         fs[i+1] = f_old
-    print(f"grad max: {grad.max()}, grad min: {grad.min()}, lambda:{lambda2}")
-    print(f"starting f {fs[0]}, closing f {f_old}")
+    # ipdb.set_trace()
+    # print(f"grad max: {grad.max()}, grad min: {grad.min()}, lambda:{lambda2}")
+    # print(f"starting f {fs[0]}, closing f {f_old}")
     return a, changes
 
 
@@ -499,9 +499,10 @@ def test_solve_for_a_and_q(t=1000):
 
     a_ = np.zeros((m, m * p))
     a_[:] = 0 * np.reshape(np.swapaxes(a, 0, 1), (m, m * p))
-    q_ = 0.1 * q.copy()
-    for _ in range(20):
-        a_, changes = solve_for_a(q_, s1, s2, a_, lambda2=0.01, max_iter=1000, tol=1e-8)
-        q_ = solve_for_q(q_, s3, s1, s2, a_, lambda2=0.01)
+    q_ = 1 * q.copy()
+    for _ in range(100):
+        a_, changes = solve_for_a(q_, s1, s2, a_, p, lambda2=0.1, max_iter=1000, tol=1e-8)
+        q_ = solve_for_q(q_, s3, s1, s2, a_, lambda2=0.1)
         # ipdb.set_trace()
+    warnings.filterwarnings('ignore')
     ipdb.set_trace()